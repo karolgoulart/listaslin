187babce4c8c57768cc21e120da4bc9792f90d40
Anne Karoline Goulart <annekgoulart@gmail.com>
2025-11-17 22:12:41 -0300

    Format code for consistency in encadeadas2.c

--- encadeadas2.c
+++ encadeadas2.c
@@ -16,18 +16,18 @@ int main (){
     caixa c1,c2,c3,c4,c5;
 
     c1.valor= 1;
-		c2.valor= 3;
-		c3.valor= 9;
-		c4.valor= 7;
-		c5.valor= 5;
+	c2.valor= 3;
+	c3.valor= 9;
+	c4.valor= 7;
+	c5.valor= 5;
 
-		c1.prox= &c2;
-		c2.prox= &c3;
-		c3.prox= &c4;
-		c4.prox= &c5;
-		c5.prox= NULL;
+	c1.prox= &c2;
+	c2.prox= &c3;
+	c3.prox= &c4;
+	c4.prox= &c5;
+	c5.prox= NULL;
 
-		caixa* inicio= &c1;
-		exibe (inicio);
-		return 0;
+	caixa* inicio= &c1;
+	exibe (inicio);
+	return 0;
 }
